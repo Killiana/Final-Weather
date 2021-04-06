52c6d43415bd6ab40deb20ca57be22380108f057
Killiana <81246683+Killiana@users.noreply.github.com>
2021-04-06 14:52:53 +0100

    Added relevant icon

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
+function getDay(timestamp){
+  let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10){
+ return `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10){
+  return `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+let day  = days[date.getDay()];
 
-function currentTemp(response){
-    console.log(response.data);
-    let temperatureEl = document.querySelector("#temperature");
-    let cityName = document.querySelector("#city");
-    let description = document.querySelector("#description");
-    let humidityEl = document.querySelector("#humidity");
-    let humidity = response.data.main.humidity;
-    let windEl = document.querySelector("#wind");
-    let wind = Math.round(response.data.wind.speed);
-    windEl.innerHTML = `Wind: ${wind}mph`;
-    humidityEl.innerHTML = `Humidity: ${humidity}%`;
-    description.innerHTML = response.data.weather[0].description;
-    temperatureEl.innerHTML = Math.round(response.data.main.temp);
-    cityName.innerHTML = response.data.name;
+
+return `${day} ${hours}:${minutes}`;
 }
 
-let apiKey = "5d480a9ea4973e7dfcb6ca4444c1582f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=${apiKey}&units=metric`;
+function getCity(response){
+    console.log(response.data);
+    let temperature = document.querySelector("#temperature");
+    let wind = document.querySelector("#wind");
+    let humidity = document.querySelector("#humidity");
+    let description = document.querySelector("#description");
+    let city = document.querySelector("#city");
+    let day = document.querySelector("#day");
+    let tempEl = Math.round(response.data.main.temp);
+    let windEl = Math.round(response.data.wind.speed);
+    let iconEl = document.querySelector("#icon");
+    temperature.innerHTML = `${tempEl}`;
+    wind.innerHTML = `${windEl}`;
+    humidity.innerHTML = response.data.main.humidity;
+    description.innerHTML = response.data.weather[0].description;
+    city.innerHTML = response.data.name;
+    day.innerHTML = getDay(response.data.dt * 1000);
+    iconEl.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconEl.setAttribute("alt", response.data.weather[0].description);
+}
 
-axios.get(apiUrl).then(currentTemp);
+
+let apiKey = "5d480a9ea4973e7dfcb6ca4444c1582f";
+let city = `London`;
+let units = `metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(getCity);
